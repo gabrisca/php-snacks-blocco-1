600680c393edb214dfc11ae00f7d9da505757774
gabrisca <gabrielescarparo1985@gmail.com>
2021-06-07 12:13:54 +0200

    snack ok

---
+++ snack6.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    .grey {
+      background-color: grey;
+    }
+
+    .green {
+      background-color: green;
+    }
+  </style>
+</head>
+
+<body>
+  <h1>Snack 6</h1>
+  <h4>
+    Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde. <br>
+
+    <?php
+
+    $db = [
+      'teachers' => [
+        [
+          'name' => 'Michele',
+          'lastname' => 'Papagni'
+        ],
+        [
+          'name' => 'Fabio',
+          'lastname' => 'Forghieri'
+        ]
+      ],
+      'pm' => [
+        [
+          'name' => 'Roberto',
+          'lastname' => 'Marazzini'
+        ],
+        [
+          'name' => 'Federico',
+          'lastname' => 'Pellegrini'
+        ]
+      ]
+    ];
+
+    // stampo l'array
+    var_dump($db);
+    ?>
+
+
+    <?php
+    foreach ($db as $key => $user) {
+      echo "<h2>$key</h2>";
+      for ($i = 0; $i < count($user); $i++) {
+        echo "{$user[$i]['name']} {$user[$i]['lastname']}<br>";
+      };
+    }
+    ?>
+
+    <!-- creo un div con due ul distinti -->
+    <div class="grey">
+      <ul>
+        <?php
+        foreach ($db['teachers'] as $value) { 
+        ?>
+          <li>
+            <?php 
+              echo $value['name'] ?> <?php $value['lastname'] 
+            ?>
+          </li>
+        <?php } ?>
+      </ul>
+    </div>
+    <div class="green">
+      <ul>
+        <?php
+        foreach ($db['pm'] as $value) { ?>
+          <li>
+            <?php echo $value['name'] ?> <?php $value['lastname'] ?>
+          </li>
+        <?php } ?>
+      </ul>
+    </div>
+
+
+
+</body>
+
+</html>
---
+++ snack7.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>Snack7</h1>
+  <h5>
+    Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. <br>
+    Stampare Nome, Cognome e la media dei voti di ogni alunno.
+  </h5>
+
+  <?php
+  $students = [
+    [
+      'name' => 'Roberto',
+      'lastname' => 'Marazzini',
+      'votes' => [6, 6, 7, 8, 4, 7]
+    ],
+    [
+      'name' => 'Flavia',
+      'lastname' => 'Iola',
+      'votes' => [8, 8, 7, 8, 8, 7]
+    ],
+    [
+      'name' => 'Camilla',
+      'lastname' => 'Barbagianni',
+      'votes' => [9, 9, 9, 9, 9, 9]
+    ],
+    [
+      'name' => 'Cosimo',
+      'lastname' => 'De Cosimi',
+      'votes' => [5, 6, 6, 5, 5, 6]
+    ],
+    [
+      'name' => 'Leonida',
+      'lastname' => 'Dei Poveri',
+      'votes' => [7, 6, 7, 5, 7, 7]
+    ],
+  ];
+
+  var_dump($students)
+  ?>
+
+  <ul>
+    <?php
+    foreach ($students as $key => $student) { ?>
+      <li>
+        <?php echo $student['name'] // stampo il nome di ogni studente?>
+        <?php echo $student['lastname'] // stampo il cognome di ogni studente?>
+         - Media voti:
+        <?php
+        // number_format formatta i numeri
+        echo number_format(array_sum($student['votes']) / count($student['votes']), 2)
+        ?>
+      </li>
+    <?php } ?>
+  </ul>
+</body>
+
+</html>
